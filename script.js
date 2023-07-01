72476f5befd2fabf046d52d7fd29e366cb1a82ed
Adam <surzyn.adam@gmail.com>
2023-07-01 17:13:04 +0200

    Adding pages function

--- script.js
+++ script.js
@@ -1,22 +1,103 @@
-const currentPage = 1;
+let currentPage = 1;
 const rnmUrlCharacters = "https://rickandmortyapi.com/api/character";
 //Change the visible page depending on the global page variable -> make a css class with display:hidden
 
-const rickApi = async () => {
-  const response = await fetch(rnmUrlCharacters);
-  const rnmJson = await response.json();
-  return rnmJson;
-};
+class rnmChars {
+  constructor(url) {
+    this.url = url;
+    //Replace this.list with a variable, and pass it as an argument
+    this.list = document.querySelector(".list-of-chars");
+  }
 
-rickApi().then((chars) => {
-  const list = document.querySelector(".list-of-chars");
-  chars.results.forEach((char) => {
-    let newElement = document.createElement("li");
-    newElement.innerHTML = char.name.toString();
-    list.appendChild(newElement);
-  });
-});
+  async getRnmData() {
+    const response = await fetch(this.url);
+    const rnmJson = await response.json();
+    return rnmJson;
+  }
 
+  clearContent() {
+    this.list.replaceChildren();
+  }
+
+  async addContentToPage() {
+    let newData = await this.getRnmData();
+    newData.results.map((char) => {
+      let newElement = document.createElement("li");
+      newElement.innerHTML = char.name.toString();
+      this.list.appendChild(newElement);
+    });
+  }
+
+  switchPage(el) {
+    let fromPage = el.srcElement.innerText;
+    console.log(el.srcElement.innerText);
+    if (currentPage === 1) {
+      this.url = this.url + `?page=${currentPage}`;
+    } else if (currentPage > 1) {
+      this.url = this.url.replace(`?page=${currentPage}`, `?page=${fromPage}`);
+      currentPage = fromPage;
+    }
+    this.clearContent();
+    this.addContentToPage();
+    this.addPageElement();
+    currentPage++;
+  }
+
+  charSearch() {}
+
+  addPageElement() {
+    if (currentPage === 1) {
+      const addedContainer = document.createElement("div");
+      document.body.appendChild(addedContainer);
+      addedContainer.setAttribute("class", "pagesContainer");
+      const addedPage = document.createElement("div");
+      addedContainer.appendChild(addedPage);
+      addedPage.setAttribute("id", `page ${currentPage}`);
+      addedPage.innerHTML = currentPage.toString();
+      addedPage.addEventListener("click", (e) => {
+        this.switchPage(e);
+      });
+      addedContainer.appendChild(addedPage);
+    } else if (currentPage > 1) {
+      console.log("added");
+      const addedContainer = document.querySelector(".pagesConatainer");
+      const addedPage = addedContainer.createElement("div");
+      addedContainer.appendChild(addedPage);
+      addedPage.setAttribute("id", `page ${currentPage}`);
+      addedPage.innerHTML = currentPage.toString();
+      addedContainer.appendChild(addedPage);
+      addedPage.addEventListener("click", (e) => {
+        this.switchPage(e);
+      });
+    }
+  }
+
+  addPagesContainer() {
+    const addedContainer = document.createElement("div");
+    document.body.appendChild(addedContainer);
+    addedContainer.setAttribute("class", "pagesContainer");
+    const arrowLeft = document.createElement("div");
+    arrowLeft.setAttribute("class", "arrowL");
+    arrowLeft.innerText = "<";
+    addedContainer.appendChild(arrowLeft);
+    const arrowRight = document.createElement("div");
+    arrowRight.setAttribute("class", "arrowR");
+    arrowRight.innerText = ">";
+    addedContainer.appendChild(arrowRight);
+    const counter = document.createElement("div");
+    counter.setAttribute("class", "counter");
+    counter.innerHTML = currentPage;
+    addedContainer.appendChild(counter);
+
+    addedPage.addEventListener("click", (e) => {
+      this.switchPage(e);
+    });
+  }
+}
+
+let chars = new rnmChars(rnmUrlCharacters);
+chars.addContentToPage();
+chars.addPageElement();
 /*To Do:
 1)Make a class for each character in the response
     -new Char will be created for each entry in the "results"
